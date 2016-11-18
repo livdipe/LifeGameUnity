c3303dd369cbcee4e0b514e4e9684e21865f984d
lcl <livdipe@gmail.com>
2016-11-18 12:23:22 +0800

    使用Shader传递数组的方式画格子

---
+++ Assets/Scripts/LifeGame.cs
@@ -0,0 +1,155 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LifeGame : MonoBehaviour 
+{
+	int row = 8;
+	int col = 8;
+	int[,] states;
+	int[,] cacheStates;
+	public Material material;
+	void Start()
+	{
+		InitShader();
+		InitStates();
+		InitColor();
+	}
+
+	void Update()
+	{
+		if (Time.frameCount % 20 == 0)
+		{
+			UpdateState();
+		}
+	}
+
+	void InitShader() 
+	{
+		int total = row * col;
+		material.SetFloat("_BoardSize", row);
+		material.SetFloat("_BlockSize", 1.0f / row);
+		material.SetFloat("_TotalBlock", total);
+		for (int i = 0; i < total; i++)
+		{
+			material.SetFloat("_Vals" + i.ToString(), 0);
+		}
+	}
+
+	void InitStates()
+	{
+		states = new int[row, col];
+		cacheStates = new int[row, col];
+		for (int i = 0; i < row; i++)
+		{
+			for (int j = 0; j < col; j++)
+			{
+				if (Random.value < 0.2f)
+				{
+					cacheStates[i, j] = states[i, j] = 1;
+				}
+				else
+				{
+					cacheStates[i,j] = states[i, j] = 0;
+				}
+			}
+		}
+	}
+
+	void InitColor()
+	{
+		for (int i = 0; i < row; i++)
+		{
+			for (int j = 0; j < col; j++)
+			{
+				SetColor(i, j);
+			}
+		}
+	}
+
+	void SetColor(int i, int j)
+	{
+		material.SetFloat("_Vals" + (i * col + j).ToString(), states[i,j]);
+	}
+
+	void UpdateState()
+	{
+		for (int i = 0; i < row; i++)
+		{
+			for (int j = 0; j < col; j++)
+			{
+				int cnt = GetAroundLife(i, j);
+				if (cnt == 3)
+				{
+					cacheStates[i,j] = 1;
+				}
+				else if(cnt == 2)
+				{
+					cacheStates[i,j] = states[i,j];
+				}
+				else
+				{
+					cacheStates[i,j] = 0;
+				}
+			}
+		}
+		for (int i = 0; i < row; i++)
+		{
+			for (int j = 0; j < col; j++)
+			{
+				if (states[i,j] != cacheStates[i,j])
+				{
+					states[i,j] = cacheStates[i,j] ;
+					SetColor(i, j);
+				}
+			}
+		}
+	}
+
+	// 获取周围存活的细胞数量
+	int GetAroundLife(int r, int c)
+	{
+		int cnt = 0;
+		// 左上
+		if (r - 1 >=0 && c - 1 >= 0)
+		{
+			cnt += states[r - 1, c - 1];
+		}
+		// 上
+		if (r - 1 >=0)
+		{
+			cnt += states[r - 1, c];
+		}
+		// 右上
+		if (r - 1 >=0 && c + 1 < col)
+		{
+			cnt += states[r - 1, c + 1];
+		}
+		// 左
+		if (c - 1 >= 0)
+		{
+			cnt += states[r, c-1];
+		}
+		// 右
+		if (c + 1 < col)
+		{
+			cnt += states[r, c + 1];
+		}
+		// 左下
+		if (r + 1 < row && c - 1 >= 0)
+		{
+			cnt += states[r + 1, c - 1];
+		}
+		// 上
+		if (r + 1 < row)
+		{
+			cnt += states[r + 1, c];
+		}
+		// 右上
+		if (r + 1 < row && c + 1 < col)
+		{
+			cnt += states[r + 1, c + 1];
+		}
+		return cnt;
+	}
+}
---
+++ Assets/Shaders/Board.shader
@@ -0,0 +1,49 @@
+﻿Shader "Custom/Board" 
+{
+	Properties 
+	{
+		_ColorA ("ColorA", Color) = (1,1,1,1)
+		_ColorB ("ColorB", Color) = (0,1,0,1)
+	}
+	
+	SubShader 
+	{
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert_img
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+			
+			half4 _ColorA;
+			half4 _ColorB;
+			uniform half _BoardSize = 8;
+			uniform half _BlockSize = 0.125;
+			uniform half _TotalBlock = 0;
+			uniform half _Vals[64];
+			
+			half4 frag(v2f_img i) : SV_Target
+			{
+				half4 colour = _ColorA;
+				if (_TotalBlock > 0)
+				{
+					half row = floor(i.uv.y / _BlockSize);
+					half col = floor(i.uv.x / _BlockSize);
+					int idx = round(row * _BoardSize  + col);
+					
+					if (idx < _TotalBlock)
+					{
+						if (_Vals[idx] > 0)
+						{
+							colour = _ColorB;
+						}
+				    }
+				}
+
+				return colour;
+			}
+			ENDCG
+		}
+	} 
+}
